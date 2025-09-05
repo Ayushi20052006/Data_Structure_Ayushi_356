793d6ee10e604bccaa87c9d9d5a4d11cb37d2b4e
Ayushi20052006 <ayushi.jha20052006@gmail.com>
2025-09-05 12:02:49 +0530

    Sorting and Searching in vectors

--- STL.cpp
+++ STL.cpp
@@ -42,13 +42,19 @@ int main(){
     cout<<value<<" ";
    } 
       cout<<endl;
-//    Vec.pop_back();
+
+    //REMOVE LAST VALUE FROM VECTOR  TC=O(1)
+//    Vec.pop_back();   
 //    for(auto value:Vec){
 //     cout<<value<<" ";
 //    } 
 //    cout<<endl;
 //    cout<<"First value: "<<Vec.front()<<endl;
 //    cout<<"Last value: "<<Vec.back()<<endl; 
+
+    cout<<Vec.empty()<<endl;   //Returns 0(false) or 1(true) 
+
+
     //INSERTING ELEMENT IN STARTING OF VECTOR  
     Vec.insert(Vec.begin(),2);
     Vec.insert(Vec.begin()+2,10);
@@ -61,13 +67,14 @@ int main(){
    cout<<endl;    
     //cout<<"Size of vector: "<<Vec.size()<<endl;
     //cout<<"Capacity of vector: "<<Vec.capacity()<<endl;
+//TO ERASE 1ST ELEMENT OF VECTOR
    Vec.erase(Vec.begin());
    for(auto value:Vec){
     cout<<value<<" ";
    } 
    cout<<endl; 
 
-//TO ERASE A CERTAIN RANGE
+//TO ERASE A CERTAIN RANGE OF VECTOR    TC=O(N)
     Vec.erase(Vec.begin()+1,Vec.begin()+4); //to dlt elements on index 1 to 3
    for(auto value:Vec){
     cout<<value<<" ";
@@ -75,22 +82,35 @@ int main(){
    cout<<endl;  
 
 //USE OF ITERATOR
-   vector<int>::iterator itr;
-    for(auto itr=v.begin(); itr!=v.end(); itr++)
-    {
-        cout<<*(itr)<<" ";
-    }
-    cout<<endl;
+//    vector<int>::iterator itr;
+//     for(auto itr=Vec.begin(); itr!=Vec.end(); itr++)
+//     {
+//         cout<<*(itr)<<" ";
+//     }
+//     cout<<endl;
 
-    vector<int>::reverse_iterator rit;
-    for(auto rit=v.rbegin(); rit!=v.rend(); rit++)
-        cout<<*(rit)<<" ";
-    cout<<endl;
+//     vector<int>::reverse_iterator rit;
+//     for(auto rit=Vec.rbegin(); rit!=Vec.rend(); rit++)
+//         cout<<*(rit)<<" ";
+//     cout<<endl;
+//SORTING OF VECTOR
+
+    // sort(Vec.begin(),Vec.end()); //Ascending order
+    // for(auto v : Vec){
+    //     cout<<v<<" ";
+    // }
+    // cout<<endl;
+    // sort(Vec.rbegin(),Vec.rend());   //DESCENDING ORDER
+    // sort(Vec.begin(),Vec.end(),greater<int>()); //Descending order
+    // for(auto v : Vec){
+    //     cout<<v<<" ";
+    // }
+
+//BINARY SEARCH
+cout<<binary_search(Vec.begin(),Vec.end(),6)<<endl;   //returns 0 or 1
+
+//TO GET INDEX OF PARTICULAR ELEMENT 
+cout<<find(Vec.begin(),Vec.end(),8)-Vec.begin()<<endl;   
 
 
-
-
-
-
-
-}
+} 
