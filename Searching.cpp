33b52a16c3aec64f435da85649bf904bd0d84b34
Ayushi20052006 <ayushi.jha20052006@gmail.com>
2025-09-29 15:13:37 +0530

    Linear search

--- Searching.cpp
+++ Searching.cpp
@@ -1,24 +1,24 @@
 //LINEAR SEARCH
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int n, target;
-    cout<<"Enter the size of array";
-    cin>>n;
-    int arr[100];
-    for (int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    cout<<"Enter the element to be searched: ";
-    cin>>target;
-    for(int i=0;i<n;i++){
-        if(arr[i]==target){
-            cout<<target<<" found at "<<i+1<<" position"<<endl;
-            break;
-        }
-    }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n, target;
+//     cout<<"Enter the size of array";
+//     cin>>n;
+//     int arr[100];
+//     for (int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     cout<<"Enter the element to be searched: ";
+//     cin>>target;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target){
+//             cout<<target<<" found at "<<i+1<<" position"<<endl;
+//             break;
+//         }
+//     }
 
-}
+// }
 
 
 // BINARY SEARCH AFTER SORTING THE GIVEN ARRAY
